0c699544b0ffa3a36b1d7bac9b114b2d40ca7aef
Joseph Cho <jscho13@gmail.com>
2016-06-25 13:28:27 -0400

    remove materialize, save comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(params[:comment])
+    @comment = Comment.new
+    @comment.post_id = @post.id
+    @comment.body = params[:comment][:body]
+    @comment.save
     redirect_to @post
   end
 end
