c023bb24b29d9146949641d37fef570bb6ef5614
Joseph Cho <jscho13@gmail.com>
2016-06-25 13:29:22 -0400

    add comment to come back to

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
+    
+    # TODO: This block needs to become a one-liner
     @comment = Comment.new
     @comment.post_id = @post.id
     @comment.body = params[:comment][:body]
     @comment.save
+    
     redirect_to @post
   end
 end
