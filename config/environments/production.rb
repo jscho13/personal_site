53855fba5c3632d149ec4686a8cdfe8ebc5ff51a
Joseph Cho <jscho13@gmail.com>
2017-03-18 16:01:55 -0400

    use compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # The default for this is false. We set it to true because we DO want to fallback to assets pipeline.
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
