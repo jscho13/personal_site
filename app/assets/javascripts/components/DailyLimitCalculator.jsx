58002216ccd7238ea088943eac591ca0c075aaf1
Joseph Cho <jscho13@gmail.com>
2017-04-16 23:05:50 -0400

    add react portion of calculator

--- app/assets/javascripts/components/DailyLimitCalculator.jsx
+++ app/assets/javascripts/components/DailyLimitCalculator.jsx
@@ -3,29 +3,40 @@ class DailyLimitCalculator extends React.Component {
     super(props)
     this.state = {
       amount: 0,
-      days: 30,
+      days: 0,
       dsq: 0
     }
     
-    this.handleChange = this.handleChange.bind(this);
+    this.updateAllowable = this.updateAllowable.bind(this);
+    this.updateDaysLeft = this.updateDaysLeft.bind(this);
   }
   
-  handleChange(event) {
-    let newDsq = event.target.value;
+  updateAllowable(event) {
+    newAllowable = event.target.value;
+    let newDsq = newAllowable/this.state.days;
+    this.setState({amount: newAllowable, dsq: newDsq});
+  }
+  
+  updateDaysLeft(event) {
+    newDaysLeft = event.target.value;
+    let newDsq = this.state.amount/newDaysLeft;
+    this.setState({days: newDaysLeft, dsq: newDsq});
   }
   
   render() {
     return (
       <div>
         <input
+          id="allowableSpending"
           type="number"
           placeholder="Allowable Spending"
-          onChange={this.handleChange}
+          onChange={this.updateAllowable}
         />
         <input
+          id="daysLeft"
           type="number"
           placeholder="Days Left in Month"
-          onChange={this.handleChange}
+          onChange={this.updateDaysLeft}
         />
         <div>{this.state.dsq}</div>
       </div>
