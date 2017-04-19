638c6eb1b33d524cc389088be5d58a762790118b
Joseph Cho <jscho13@gmail.com>
2017-04-19 17:39:27 -0400

    style up dsq

--- app/assets/javascripts/components/DailyLimitCalculator.jsx
+++ app/assets/javascripts/components/DailyLimitCalculator.jsx
@@ -11,34 +11,52 @@ class DailyLimitCalculator extends React.Component {
     this.updateDaysLeft = this.updateDaysLeft.bind(this);
   }
   
+  componentDidMount() {
+    date = new Date();
+    daysLeft = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() - date.getDate();
+    this.setState({days: daysLeft})
+  }
+  
   updateAllowable(event) {
     newAllowable = event.target.value;
     let newDsq = newAllowable/this.state.days;
-    this.setState({amount: newAllowable, dsq: newDsq});
+    this.setState({dsq: newDsq, amount: newAllowable});
   }
   
   updateDaysLeft(event) {
     newDaysLeft = event.target.value;
     let newDsq = this.state.amount/newDaysLeft;
-    this.setState({days: newDaysLeft, dsq: newDsq});
+    this.setState({dsq: newDsq, days: newDaysLeft});
   }
   
   render() {
     return (
-      <div>
-        <input
-          id="allowableSpending"
-          type="number"
-          placeholder="Allowable Spending"
-          onChange={this.updateAllowable}
-        />
-        <input
-          id="daysLeft"
-          type="number"
-          placeholder="Days Left in Month"
-          onChange={this.updateDaysLeft}
-        />
-        <div>{this.state.dsq}</div>
+      <div className="daily-spending-calc">
+        <div>
+          <label>Allowable Spending</label>
+          <input
+            id="allowableSpending"
+            type="number"
+            placeholder="Allowable Spending"
+            onChange={this.updateAllowable}
+            className="half-width"
+          />
+        </div>
+        <div>
+          <label>Days Left</label>
+          <input
+            id="daysLeft"
+            type="number"
+            placeholder="Days Left"
+            value={this.state.days}
+            onChange={this.updateDaysLeft}
+            className="half-width"
+          />
+        </div>
+        <div>
+          <label>Spending Per Day</label>
+          <div>{this.state.dsq}</div>
+        </div>
       </div>
     )
   }
