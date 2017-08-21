8e6ec0f7f874761c7568feb1244c91461859082d
Joseph Cho <jscho13@gmail.com>
2017-08-21 00:10:46 -0400

    update migration

--- db/migrate/20170820185915_rename_transactions_to_budget_item.rb
+++ db/migrate/20170820185915_rename_transactions_to_budget_item.rb
@@ -1,6 +1,7 @@
 class RenameTransactionsToBudgetItem < ActiveRecord::Migration[5.1]
   def change
     rename_table :transactions, :budget_items
-    rename_column :budgets, :transaction_id, :budget_item_id
+    remove_column :budgets, :transaction_id, :integer
+    add_reference :budgets, :budget_item_id, foreign_key: true
   end
 end
