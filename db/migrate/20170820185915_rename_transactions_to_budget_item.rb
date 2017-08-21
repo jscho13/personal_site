305c50bf3a35806a80f7750999b48e32d151e8b8
Joseph Cho <jscho13@gmail.com>
2017-08-21 00:21:13 -0400

    reorder migration

--- db/migrate/20170820185915_rename_transactions_to_budget_item.rb
+++ db/migrate/20170820185915_rename_transactions_to_budget_item.rb
@@ -1,7 +1,7 @@
 class RenameTransactionsToBudgetItem < ActiveRecord::Migration[5.1]
   def change
+    remove_foreign_key :budgets, :transactions
     rename_table :transactions, :budget_items
-    remove_column :budgets, :transaction_id, foreign_key: true
     add_reference :budgets, :budget_item_id, :integer
   end
 end
--- db/migrate/20170820204358_add_users_to_budgets.rb
+++ db/migrate/20170820204358_add_users_to_budgets.rb
@@ -1,6 +1,6 @@
 class AddUsersToBudgets < ActiveRecord::Migration[5.1]
   def change
-    remove_column :budgets, :user_id, :integer
+    remove_column :budgets, :user_id, foreign_key: true
     
     change_table :budgets do |t|
       t.references :user
