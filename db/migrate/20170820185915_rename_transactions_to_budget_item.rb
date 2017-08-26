bd70aefcef015c55151eef12ede751f612b9f152
Joseph Cho <jscho13@gmail.com>
2017-08-26 14:33:39 -0400

    update migration to remove foreign key

--- db/migrate/20170820185915_rename_transactions_to_budget_item.rb
+++ db/migrate/20170820185915_rename_transactions_to_budget_item.rb
@@ -1,6 +1,6 @@
 class RenameTransactionsToBudgetItem < ActiveRecord::Migration[5.1]
   def change
-    remove_column :budgets, :transaction_id, :integer
+    remove_column :budgets, :transaction_id, foreign_key: true
     rename_table :transactions, :budget_items
     add_reference :budgets, :budget_item
   end
