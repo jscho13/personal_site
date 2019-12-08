93a6e256f6b6d04ac0de89e67655781f21f42689
Joseph Cho <jscho13@gmail.com>
2019-12-08 18:11:39 -0500

    Try different asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,6 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = true
-  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -25,7 +24,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # The default for this is false. We set it to true because we DO want to fallback to assets pipeline.
-  # config.assets.compile = false
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
